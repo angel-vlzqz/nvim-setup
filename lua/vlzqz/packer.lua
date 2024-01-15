fd7af8a91445e8d54e0e68c76b3eae100126ec2c
angel-vlzqz <106009767+angel-vlzqz@users.noreply.github.com>
2024-01-15 00:23:03 -0700

    install lsp-zero

--- lua/vlzqz/packer.lua
+++ lua/vlzqz/packer.lua
@@ -24,4 +24,25 @@ return require('packer').startup(function(use)
 		use('theprimeagen/harpoon')
 		use('mbbill/undotree')
 		use('tpope/vim-fugitive')
+
+		use{
+			'vonheikemen/lsp-zero.nvim',
+			requires = 
+			{
+				-- LSP Support
+				{'neovim/nvim-lspconfig'},
+				{'williamboman/mason.nvim'},
+				{'williamboman/mason-lspconfig.nvim'},
+				-- Autocompletion
+				{'hrsh7th/nvim-cmp'},
+				{'hrsh7th/cmp-nvim-lsp'},
+				{'hrsh7th/cmp-buffer'},
+				{'hrsh7th/cmp-path'},
+				-- Snippets
+				{'L3MON4D3/LuaSnip'},
+				{'saadparwaiz1/cmp_luasnip'},
+				{'hrsh7th/cmp-nvim-lua'},
+				{'rafamadriz/friendly-snippets'},
+			}
+		}
 	end)
