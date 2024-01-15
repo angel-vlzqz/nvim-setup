281b0ca7938a3068678dad3871f536bc73057604
angel-vlzqz <106009767+angel-vlzqz@users.noreply.github.com>
2024-01-15 00:50:04 -0700

    install VimBeGood

--- lua/vlzqz/packer.lua
+++ lua/vlzqz/packer.lua
@@ -4,45 +4,46 @@
 vim.cmd [[packadd packer.nvim]]
 
 return require('packer').startup(function(use)
-	-- Packer can manage itself
-	use 'wbthomason/packer.nvim'
+    -- Packer can manage itself
+    use 'wbthomason/packer.nvim'
 
-	use {
-		'nvim-telescope/telescope.nvim', tag = '0.1.5',
-		-- or                            , branch = '0.1.x',
-		requires = { {'nvim-lua/plenary.nvim'} }
-	}
-	use({
-		'rose-pine/neovim',
-		as = 'rose-pine',
-		config = function()
-			vim.cmd('colorscheme rose-pine')
-		end})
-		use('nvim-treesitter/nvim-treesitter', {run = ':TSUpdate'})
-		use('nvim-treesitter/playground')
-		use('github/copilot.vim')
-		use('theprimeagen/harpoon')
-		use('mbbill/undotree')
-		use('tpope/vim-fugitive')
+    use {
+        'nvim-telescope/telescope.nvim', tag = '0.1.5',
+        -- or                            , branch = '0.1.x',
+        requires = { {'nvim-lua/plenary.nvim'} }
+    }
+    use({
+        'rose-pine/neovim',
+        as = 'rose-pine',
+        config = function()
+            vim.cmd('colorscheme rose-pine')
+        end})
+        use('nvim-treesitter/nvim-treesitter', {run = ':TSUpdate'})
+        use('nvim-treesitter/playground')
+        use('github/copilot.vim')
+        use('theprimeagen/harpoon')
+        use('mbbill/undotree')
+        use('tpope/vim-fugitive')
 
-		use{
-			'vonheikemen/lsp-zero.nvim',
-			requires = 
-			{
-				-- LSP Support
-				{'neovim/nvim-lspconfig'},
-				{'williamboman/mason.nvim'},
-				{'williamboman/mason-lspconfig.nvim'},
-				-- Autocompletion
-				{'hrsh7th/nvim-cmp'},
-				{'hrsh7th/cmp-nvim-lsp'},
-				{'hrsh7th/cmp-buffer'},
-				{'hrsh7th/cmp-path'},
-				-- Snippets
-				{'L3MON4D3/LuaSnip'},
-				{'saadparwaiz1/cmp_luasnip'},
-				{'hrsh7th/cmp-nvim-lua'},
-				{'rafamadriz/friendly-snippets'},
-			}
-		}
-	end)
+        use{
+            'vonheikemen/lsp-zero.nvim',
+            requires = 
+            {
+                -- LSP Support
+                {'neovim/nvim-lspconfig'},
+                {'williamboman/mason.nvim'},
+                {'williamboman/mason-lspconfig.nvim'},
+                -- Autocompletion
+                {'hrsh7th/nvim-cmp'},
+                {'hrsh7th/cmp-nvim-lsp'},
+                {'hrsh7th/cmp-buffer'},
+                {'hrsh7th/cmp-path'},
+                -- Snippets
+                {'L3MON4D3/LuaSnip'},
+                {'saadparwaiz1/cmp_luasnip'},
+                {'hrsh7th/cmp-nvim-lua'},
+                {'rafamadriz/friendly-snippets'},
+            }
+        }
+        use('theprimeagen/vim-be-good')
+    end)
