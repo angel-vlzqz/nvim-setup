5a5a1a1d8a2a3f7c38eb1c1cdb6a190b617ecb76
angel-vlzqz <106009767+angel-vlzqz@users.noreply.github.com>
2024-01-15 00:03:33 -0700

    install figutive: may be removed tbh

--- plugin/packer_compiled.lua
+++ plugin/packer_compiled.lua
@@ -119,6 +119,11 @@ _G.packer_plugins = {
     loaded = true,
     path = "/Users/vlzqz/.local/share/nvim/site/pack/packer/start/undotree",
     url = "https://github.com/mbbill/undotree"
+  },
+  ["vim-fugitive"] = {
+    loaded = true,
+    path = "/Users/vlzqz/.local/share/nvim/site/pack/packer/start/vim-fugitive",
+    url = "https://github.com/tpope/vim-fugitive"
   }
 }
 
